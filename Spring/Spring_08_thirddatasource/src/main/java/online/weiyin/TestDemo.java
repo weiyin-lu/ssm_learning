61c0fc071296b379f6e0e1a4b1187f65c5b0baed
weiyin-lu <weiyin2002@outlook.com>
2023-01-10 17:58:52 +0800

    Spring 第三方源（配置导入）

--- Spring/Spring_08_thirddatasource/src/main/java/online/weiyin/TestDemo.java
+++ Spring/Spring_08_thirddatasource/src/main/java/online/weiyin/TestDemo.java
@@ -2,9 +2,12 @@ package online.weiyin;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+
 public class TestDemo {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+        DataSource source = (DataSource) context.getBean("source");
+        System.out.println(source);
     }
 }
