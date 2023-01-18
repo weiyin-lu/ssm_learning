2c522815694baadcad7dae13288b62994421c523
weiyin-lu <weiyin2002@outlook.com>
2023-01-18 14:10:30 +0800

    SpringMVC RESTful注解快速开发

--- SpringMVC/SpringMVC_05_restful/src/main/java/online/weiyin/controller/UserController.java
+++ SpringMVC/SpringMVC_05_restful/src/main/java/online/weiyin/controller/UserController.java
@@ -4,30 +4,30 @@ import online.weiyin.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/books") //RESTful风格，起始路径用复数形式
-@ResponseBody //代表类中所有方法的响应都返回到响应体里
-public class BookController {
+@RestController //包括@Controller和@ResponseBody的所有功能
+@RequestMapping("/users") //RESTful风格，起始路径用复数形式
+public class UserController {
 
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @PostMapping("/add/") //使用更简洁的方式声明请求类型，前提是必须使用RestController注解
     public String add(@RequestBody User user) {
         System.out.println("添加" + user);
         return "add";
     }
 
-    @RequestMapping(value = "/select/{i}",method = RequestMethod.GET)
+    @GetMapping("/select/{i}")
     public String select(@PathVariable int i) {
         System.out.println("查询" + i);
         return "select";
     }
 
-    @RequestMapping(value = "/update",method = RequestMethod.PUT)
+
+    @PutMapping("/update/")
     public String update(@RequestBody User user) {
         System.out.println("更新" + user);
         return "update";
     }
 
-    @RequestMapping(value = "/delete/{i}",method = RequestMethod.DELETE)
+    @DeleteMapping("/delete/{i}")
     public String delete(@PathVariable int i) {
         System.out.println("删除" + i);
         return "delete";
